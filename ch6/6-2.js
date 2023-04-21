3fbcaa3fa8c6380200c0277470aca14d899b8eb1
김명욱 <anko@gsitm.com>
2023-04-21 12:47:34 +0900

    함수 인라인하기

--- ch6/6-2.js
+++ ch6/6-2.js
@@ -1,20 +1,24 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  // return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  
+  // 굳이 함수로뺄 필요가 없어서 인라인 처리
+  return driver.numberOfLateDeliveries > 5;
+  
 }
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
-}
+// function moreThanFiveLateDeliveries(dvr) {
+//   return dvr.numberOfLateDeliveries > 5;
+// }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
 
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
-}
+reportLines({
+  name: 'anko',
+  location: 'seoul',
+});
