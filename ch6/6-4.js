e333d809d4dba2fdcefe400507fb2f4088c8981b
김명욱 <anko@gsitm.com>
2023-04-27 12:14:21 +0900

    변수 추출 및 인라인 처리

--- ch6/6-4.js
+++ ch6/6-4.js
@@ -1,4 +1,9 @@
+// export function isDeliveryFree(anOrder) {
+//   let basePrice = anOrder.basePrice;
+//   return basePrice > 1000;
+// }
+
+// 굳이 변수로 추출할 필요가 없어서 인라인 처리
 export function isDeliveryFree(anOrder) {
-  let basePrice = anOrder.basePrice;
-  return basePrice > 1000;
+  return anOrder.basePrice > 1000;
 }
