4753b85245176597905e67045340b6200364bd2a
it1403 <anko@gsitm.com>
2023-05-22 14:30:50 +0900

    함수 호출 위치 변경

--- ch8/8-4.js
+++ ch8/8-4.js
@@ -2,6 +2,8 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -10,6 +12,7 @@ function listRecentPhotos(outStream, photos) {
     .forEach((p) => {
       outStream.write('<div>\n');
       emitPhotoData(outStream, p);
+      outStream.write(`<p>위치!: ${photos.location}</p>\n`);
       outStream.write('</div>\n');
     });
 }
@@ -17,7 +20,6 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
